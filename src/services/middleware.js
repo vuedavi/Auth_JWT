606fd5cb1484b07b699c40df4a94d89c85ba8eea
Luis Davi <luisdavi.moz@outlook.com>
2022-01-21 22:49:24 -0300

    Remove log

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie"
 
 export default {
     Auth(to, from, next){
-        console.log(to, from, next)
+        
         const Token = Cookies.get('_myapp_Token')
         if(!Token){
             return next('/')
